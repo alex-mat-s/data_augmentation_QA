3ce6fc552163ef85b22f3dafed5291e20c8a2c5c
alex-mat-s <alexandra.matveeva.s@yandex.ru>
2023-11-30 03:55:14 +0300

    EDA for base dataset

---
+++ src/data_preprocessor.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import re
+
+
+def clear_data(df):
+    df["question"] = [elem.replace('Вопрос:', '') for elem in df["question"]]
+    df["answer"] = [elem.replace('Резюме:', '') for elem in df["answer"]]
+    df["answer"] = [elem.replace('Краткое описание:', '') for elem in df["answer"]]
+
+    # Remove all samples with insignificant context
+    ids = []
+    substr = "На этой странице Основные сведения"
+    for i in range(df.shape[0]):
+        if substr not in df.iloc[i]["context"]:
+            ids.append(i)
+    df = df.iloc[ids,:]
+
+    for col in df.columns:
+        if col == "answer_start":
+            continue
+        df[col] = [re.sub('[-—\n\t]', '', elem) for elem in df[col]]
+        df[col] = [re.sub(' +', ' ', elem) for elem in df[col]]
+        # Lowercase
+        df[col] = [elem.lower() for elem in df[col]]
+    
+    return df
+
+    
